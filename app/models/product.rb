d6cb144bcc8195add0e402f873bf85cafd469f89
Tomáš Matějka <mtk.eworkers@gmail.com>
2014-06-21 01:04:57 +0200

    Zlepseny view.Konec iteracky :)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_that_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => 
+  {
+    :with   => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
   attr_accessible :description, :image_url, :price, :title
 end
