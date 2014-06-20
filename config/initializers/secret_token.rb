e7f93cb044237140cec04ce592d96c59fa546845
Tomáš Matějka <matka@MTK-NTBK-LNX.site>
2014-06-20 22:50:52 +0200

    Prvni nahozeni

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1b360ff1fc4d5bb6c6ab704ba9dda661d7ab26fcd5fbc2a1c4d9ee2699bcda105dbc2acb9e343b964533c06ab8f71f5fa3a2e9623c5ccc5bffaed53d66e66c59'
